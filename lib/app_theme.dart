398816cc254be86ff5ff2cfa006cd057f0e8b6e3
COMPUMARTS <@Aya-Elsayed-Mahmoud>
2024-07-26 14:11:43 +0300

    Implement quran tab.

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -7,11 +7,26 @@ class AppTheme {
   static const Color white = Color(0xFFF8F8F8);
   static const Color black = Color(0xFF242424);
   static ThemeData lightTheme = ThemeData(
-      scaffoldBackgroundColor: Colors.transparent,
+    primaryColor: lightprimary,
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      titleTextStyle:
+          TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: black),
+      centerTitle: true,
+    ),
+    scaffoldBackgroundColor: Colors.transparent,
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
         type: BottomNavigationBarType.fixed,
-        unselectedItemColor: Colors.amber,
+      backgroundColor: lightprimary,
+      unselectedItemColor: Colors.amber,
         selectedItemColor: Colors.black,
-      ));
+    ),
+    textTheme: TextTheme(
+      headlineSmall:
+          TextStyle(fontSize: 25, fontWeight: FontWeight.w400, color: black),
+      titleLarge:
+          TextStyle(fontSize: 20, fontWeight: FontWeight.w400, color: black),
+    ),
+  );
   static ThemeData darkTheme = ThemeData();
 }
--- lib/tabs/sebha/sebha_tab.dart
+++ lib/tabs/sebha/sebha_tab.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 class SebhaTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
-    );
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Column(children: [
+        Image.asset(
+          'assets/images/head_sebha_logo.png',
+        ),
+        Image.asset(
+          'assets/images/body_sebha_logo.png',
+        ),
+        Text('')
+      ])
+    ]);
   }
 }
